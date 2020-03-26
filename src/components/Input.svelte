352507fff58544cbff043a97c7818bf625d61ebb
Rajat <rajatj@geekyants.com>
2020-03-26 16:22:04 +0530

    Svelte Skeleton

---
+++ src/components/Input.svelte
@@ -0,0 +1,9 @@
+<script>
+  //   import Button, { Label } from "@smui/button";
+  //   export let name = "";
+  //   export let label = "";
+</script>
+
+<!-- <Button>
+  <Label>Do Something</Label>
+</Button> -->
---
+++ src/layout/Login.svelte
@@ -0,0 +1,7 @@
+<script>
+  //   import Input from "../components/Input.svelte";
+</script>
+
+<div>
+  <!-- <Input /> -->
+</div>
